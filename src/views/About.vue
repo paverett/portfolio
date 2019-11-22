bc2dd0099d7280b0f7b96a4982bcccc3a8abceab
Porter Averett <paverett@email.arizona.edu>
2019-11-21 17:43:31 -0700

    fixed a few typos

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,37 +1,6 @@
 <template>
   <div class="about">
     <h1>Porter Averett</h1>
-    <p>
-      Currently a Senior at the University of Arizona studying a BS in Information Science and Technology.
-      Growing up I always had a passion for working with computers, it wasnt until my after my first year in college I found out 
-       what I really wanted to do. Software Engineering.
-    </p>
-    <p>
-      In an Information Science an Technology degree one has many options of what to study, from game design, to web development, and the beginnings of data science.
-      I studied the beginnnings of data science and web development. I learned HTML/CSS and Javascript, as well as python, numpy, pandas,
-      and more scientific python packages. as well as how to plot beautiful graphs with matplotlib. Other core concepts included bayesian modeling, decision trees and random forest regressors. 
-      The main comcepts I took from these are how to get meaningful data out of raw data and how to design and the importance of CSS and not to solely rely of Javascript.
-    </p>
-    <p>
-      Not only have I learned quite a bit from my studies but I also learned quite a bit from my Software Engineer Internship at American Express.
-      I was placed on a team as a front end engineer intitially. I had to quickly get up to speed on React as I had never had experience with it before.
-      By the end of the second week I feel I had a decent grasp on the concepts. Then they introduced redux... I spent more time learning about redux in order to 
-      incorporate state given from an API. After completing this project I wanted to get some experience with APIs and middleware. I had to quickly get up to speed on Java Spring boot.
-      I rebuilt an API so that it could be used just for intranet use to test my newly created react component. Wrapping up my internship I had to give a presentation 
-      in front of the entire team as well as the VP of our overarching team. I feel I never stopped learning while I was there and I felt the same for everyone who worked there.
-      It was a wonderful experience and I learned alot from it.
-    </p>
-    <p>
-      I started a few personal project in my last semester as well as a Capstone project as a culmination of my studies. My capstone project can be seen in my portfolio. 
-      It is a full stack Django application with Postgres as our Database. A user can scan upc codes from food they buy and then enter a quantity and expiration date. It wll then store these items 
-      in our database. Our application will then recommend recipes for users based on expiring items and how many items these recipes use to help combat food waste. Users can save recipes, remove products from their pantry and our application will automatically remove items after they hit the expiration date.
-      My second project is an android application built with Flutter and Dart that will convert all units of the same category at the same time. SO all units of distance can be seen at the same time. If a unit is switched the other units are calculated based on the new unit. Still currently under development but the source can be seen on github.
-      My third project is a web app built with Vue, Golang, Python and MongoDB. My python script runs and grabs products from many different websites and uploads them to my database. My api is written in Golang and will output the priducts based on a number of filters. MongoDB is my database and holds all the products in JSON format. Go can encode and decode the json to return json in my api.
-      I chose Vue as I wanted to learn something new and it was beginner friendly. It is almost done and will be available on xoswardrobe.com, fair warning the products are lingerie. Lingerie you ask, well when I was shopping for lingerie I had a hard time switching between all the different stores and wanted to be able to view them all at once, so that is why I wrote this little web app.
-    </p>
-    <p>
-      Currently I am looking for a software engineering postion, where I can contributemy ideas and make and impact on the team.
-      I wish to never stop learing and strengthening my skills.
-    </p>
+    <p>Coming soon</p>
   </div>
 </template>
