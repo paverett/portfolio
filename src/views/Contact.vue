9e9944d0462d9103321ab3c69e18396170bab2cc
Porter Averett <paverett@email.arizona.edu>
2019-11-23 09:16:45 -0700

    changing form name

---
+++ src/views/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <form name="contactme" method="post" data-netlify="true">
+    <input type="hidden" name="form-name" value="contactme" />
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name" />'
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" />
+    <label for="message">Message:</label>
+    <textarea type="text" name="message" id="message" />
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "contact-me"
+};
+</script>
